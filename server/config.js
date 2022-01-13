da737816684ae73d9ea98f0794cddbd11182b6ab
yanhan <yanhan@rongcloud.cn>
2022-01-13 19:09:35 +0800

    fist commit

---
+++ server/config.js
@@ -0,0 +1,60 @@
+/**
+ * 奖品设置
+ * type: 唯一标识，0是默认特别奖的占位符，其它奖品不可使用
+ * count: 奖品数量
+ * title: 奖品描述
+ * text: 奖品标题
+ * img: 图片地址
+ */
+const prizes = [
+  {
+    type: 1,
+    count: 2,
+    text: "一等奖",
+    title: "",
+    img: "../img/secrit.jpg"
+  },
+  {
+    type: 2,
+    count: 5,
+    text: "二等奖",
+    title: "",
+    img: "../img/1.png"
+  },
+  {
+    type: 3,
+    count: 6,
+    text: "三等奖",
+    title: "",
+    img: "../img/2.png"
+  },
+  {
+    type: 4,
+    count: 6,
+    text: "四等奖",
+    title: "",
+    img: "../img/3.png"
+  }
+];
+
+/**
+ * 一次抽取的奖品个数与prizes对应
+ */
+const EACH_COUNT = [ 2, 5, 6, 6];
+
+/**
+ * 卡片公司名称标识
+ */
+
+module.exports = {
+  prizes,
+  EACH_COUNT,
+  SERVER_PORT: '8586',
+  DB_NAME: 'lottery',
+  DB_USER: 'root',
+  DB_PASSWORD: '123456',
+  DB_HOST: '127.0.0.1',
+  DB_PORT: '3306',
+  API_LIMITED_TIME: 10,
+  API_LIMITED_COUNT: 50
+};
---
+++ web/src/lottery/prizeList.js
@@ -0,0 +1,112 @@
+let defaultType = 0;
+
+let prizes;
+
+let prizeElement = {},
+  lasetPrizeIndex = 0;
+
+function setPrizes(pri) {
+  prizes = pri;
+  lasetPrizeIndex = pri.length - 1;
+}
+
+function showPrizeList(currentPrizeIndex) {
+  let currentPrize = prizes[currentPrizeIndex]; //${item.type == currentPrize.type ? "shine" : ""}
+  let htmlCode = `<div class="prize-mess">正在抽取<label id="prizeType" class="prize-shine">${currentPrize.text}</label>，剩余<label id="prizeLeft" class="prize-shine">${currentPrize.count}</label>个</div><ul class="prize-list">`;
+  prizes.forEach(item => {
+    if (item.type === defaultType) {
+      return true;
+    }
+    htmlCode += `<li id="prize-item-${item.type}" class="prize-item">
+                  <span></span><span></span><span></span><span></span>
+                  <div class="prize-img">
+                    <img src="${item.img}">
+                  </div>
+                  <div class="prize-text">
+                    <h5 class="prize-title">${item.text}</h5>
+                    <div class="prize-count">
+                      <div class="progress">
+                        <div id="prize-bar-${item.type}" class="progress-bar progress-bar-danger progress-bar-striped active" style="width: 100%;"></div>
+                      </div>
+                      <div id="prize-count-${item.type}" class="prize-count-left">${item.count + "/" + item.count}</div>
+                    </div>
+                  </div>
+              </li>`;});
+  htmlCode += `</ul>`;
+
+  document.querySelector("#prizeBar").innerHTML = htmlCode;
+}
+
+function resetPrize(currentPrizeIndex) {
+  prizeElement = {};
+  lasetPrizeIndex = currentPrizeIndex;
+  showPrizeList(currentPrizeIndex);
+}
+
+let setPrizeData = (function () {
+  return function (currentPrizeIndex, count, isInit) {
+    let currentPrize = prizes[currentPrizeIndex],
+      type = currentPrize.type,
+      elements = prizeElement[type],
+      totalCount = currentPrize.count;
+
+    if (!elements) {
+      elements = {
+        box: document.querySelector(`#prize-item-${type}`),
+        bar: document.querySelector(`#prize-bar-${type}`),
+        text: document.querySelector(`#prize-count-${type}`)
+      };
+      prizeElement[type] = elements;
+    }
+
+    if (!prizeElement.prizeType) {
+      prizeElement.prizeType = document.querySelector("#prizeType");
+      prizeElement.prizeLeft = document.querySelector("#prizeLeft");
+      prizeElement.prizeText = document.querySelector("#prizeText");
+    }
+
+    if (isInit) {
+      for (let i = prizes.length - 1; i > currentPrizeIndex; i--) {
+        let type = prizes[i]["type"];
+        document.querySelector(`#prize-item-${type}`).className =
+          "prize-item done";
+        document.querySelector(`#prize-bar-${type}`).style.width = "0";
+        document.querySelector(`#prize-count-${type}`).textContent =
+          "0" + "/" + prizes[i]["count"];
+      }
+    }
+
+    if (lasetPrizeIndex !== currentPrizeIndex) {
+      let lastPrize = prizes[lasetPrizeIndex],
+        lastBox = document.querySelector(`#prize-item-${lastPrize.type}`);
+      lastBox.classList.remove("shine");
+      lastBox.classList.add("done");
+      elements.box && elements.box.classList.add("shine");
+      prizeElement.prizeType.textContent = currentPrize.text;
+      // prizeElement.prizeText.textContent = currentPrize.title;
+
+      lasetPrizeIndex = currentPrizeIndex;
+    }
+
+    // if (currentPrizeIndex === 0) {
+    //   prizeElement.prizeType.textContent = "特别奖";
+    //   prizeElement.prizeText.textContent = " ";
+    //   prizeElement.prizeLeft.textContent = "不限制";
+    //   return;
+    // }
+
+    count = totalCount - count;
+    count = count < 0 ? 0 : count;
+    let percent = (count / totalCount).toFixed(2);
+    elements.bar && (elements.bar.style.width = percent * 100 + "%");
+    elements.text && (elements.text.textContent = count + "/" + totalCount);
+    prizeElement.prizeLeft.textContent = count;
+  };
+})();
+
+export {
+  showPrizeList,
+  setPrizeData,
+  setPrizes,
+  resetPrize,
+};
