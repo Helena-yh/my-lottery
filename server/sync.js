c7672ddd407e0ae2b1d0d3ba3f31ec17ff38a343
yanhan <yanhan@rongcloud.cn>
2022-01-14 09:21:18 +0800

    修改

--- server/sync.js
+++
@@ -1,7 +0,0 @@
-var ref = require('./db'), sequelize = ref.sequelize;
-// force update db
-sequelize.sync({alter: true}).then(()=>{
-    console.log('force update sucess')
-}).catch((err)=>{
-    console.log('err',err)
-})
